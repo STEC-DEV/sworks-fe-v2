fd7bc46752f2f2da99af875c0b15340ce44ce571
duke <dukebs1128@gmail.com>
2026-06-05 15:35:22 +0900

    fix : [대시보드] 차트 이슈 발생 처리6 - 우측 공지, 일정 카드 주석

--- src/app/(user)/dashboard/page.tsx
+++ src/app/(user)/dashboard/page.tsx
@@ -42,8 +42,8 @@ const DashboardPage = () => {
         {/* 왼쪽 - min-h-0 추가, 브라켓 추가 */}
         <div className="xl:flex-[8]  xl:grid xl:grid-cols-3 xl:grid-rows-5 gap-6">
           <VocTrendChart className="col-span-3 row-span-3 " />
-          {/* <TodayVocKpi className="col-start-1 row-start-4 col-span-1 row-span-1" />
-          <MonthVocKpi className="col-start-1 row-start-5 col-span-1 row-span-1" /> */}
+          <TodayVocKpi className="col-start-1 row-start-4 col-span-1 row-span-1" />
+          <MonthVocKpi className="col-start-1 row-start-5 col-span-1 row-span-1" />
 
           <QeTrendChart className="col-span-2 row-span-2" />
         </div>
@@ -51,8 +51,8 @@ const DashboardPage = () => {
         {/* 오른쪽 - 브라켓 추가 */}
         <div className="xl:flex-[3]  xl:flex xl:flex-col xl:gap-6">
           <TodayTaskChart className="flex-1" />
-          <NoticeCard className="flex-2" />
-          <ScheduleCard className="flex-3" />
+          {/* <NoticeCard className="flex-2" />
+          <ScheduleCard className="flex-3" /> */}
         </div>
       </div>
     </div>
